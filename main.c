47b59f545d8017949b1bbc7933b53d943d3303bf
Vijayvarshan <vijayvarshan4.pk@gmail.com>
2026-09-03 20:05:29 +0530

    Add main C program

---
+++ main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{   
+    // comment
+
+
+    /*
+    multi
+    line    
+    comment    
+    */
+   
+    printf("Hello, World!\n");
+    printf("second line");
+
+
+
+
+
+    return 0;
+}
