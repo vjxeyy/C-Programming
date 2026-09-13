a2db912b93bbf855d928bcbb085369d11c0d516f
Vijayvarshan <vijayvarshan4.pk@gmail.com>
2026-09-13 21:02:19 +0530

    Add shopping cart program

--- shopping_cart_program.c
+++ shopping_cart_program.c
@@ -1,17 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-// SHOPPING CART PROGRAM
+int main() {
 
+    // SHOPPING CART PROGRAM
 
-int main()
-{
-    char item[50];
-    
-    
+    char item[50] = "";
+    float price = 0.0f;
+    int quantity = 0;
+    char currency = '$';
+    float total = 0.0f;
 
+    printf("What item would you like to buy?: ");
+    fgets(item, sizeof(item), stdin);
+    item[strlen(item) - 1] = '\0';
 
+    printf("What is the price for each?: ");
+    scanf("%f", &price);
 
+    printf("How many would you like?: ");
+    scanf("%d", &quantity);
 
+    total = price * quantity;
+
+    printf("\nYou have bought %d %s\n", quantity, item);
+    printf("%c%.2f", currency, total);
 
     return 0;
 }
